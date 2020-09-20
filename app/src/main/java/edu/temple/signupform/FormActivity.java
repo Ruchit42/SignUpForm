f34087e582b25625e0dc1f6bc0ad3a476c068cf2
Ruchit Patel <tug86709@email.com>
2020-09-20 14:09:00 -0400

    Textviews work right Also the button save the information entered by the user and displays it back on the screen

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,12 +3,46 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
+    String name, email, password,Cpassword ;
+    Button save;
 
+    EditText nameInput;
+    EditText emailInput;
+    EditText passwordInput;
+    EditText conformPasswordInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.TextPersonFirstName);
+        emailInput = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        passwordInput = (EditText) findViewById(R.id.TextPassword);
+        conformPasswordInput = (EditText) findViewById(R.id.TextConformPassword);
+
+        save = (Button) findViewById(R.id.button);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+                Cpassword = conformPasswordInput.getText().toString();
+                showTest(name);
+                showTest(email);
+            }
+        });
+
+    }
+    private void showTest(String text){
+        Toast.makeText(FormActivity.this,text,Toast.LENGTH_SHORT).show();
     }
 }
